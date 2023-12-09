2c0c794f178ba985238427fc7226b6be79a6372a
Ameer Ameen <125167155+Ameer2000Mzori@users.noreply.github.com>
2023-12-09 21:50:31 +0100

    Update script.js
    Bug fix, when click on Prev button the indexCount should be carousel . Length-1

--- src/script.js
+++ src/script.js
@@ -1,38 +1,41 @@
 // selected elements
 var nextbutton = document.getElementsByClassName("next-button")[0];
 var prevbutton = document.getElementsByClassName("prev-button")[0];
-var carouselClass = document.getElementsByClassName("carousel-Class")[0];
-// gelobal varibales
+var carouselClass = document.getElementsByClassName("carousel-Class")[0]; // Typo: It should be "carouselClass" not "carousel-Class"
+
+// global variables
 var indexCount = 0;
+var countTimer = 0; // Declare countTimer here to avoid reference error
+
 // functions
 var updateImg = function () {
     var translateValue = -indexCount * 100 + "%";
-    carouselClass.style.transform = "translateX(".concat(translateValue, ")");
+    carouselClass.style.transform = "translateX(" + translateValue + ")";
 };
+
 // next img function
 var nextImageFunc = function () {
     if (indexCount < carouselClass.children.length - 1) {
         indexCount++;
-    }
-    else {
+    } else {
         indexCount = 0;
     }
     updateImg();
     countTimer = 0;
 };
+
 // prev img function
 var prevImageFunc = function () {
     if (indexCount > 0) {
         indexCount--;
-    }
-    else {
-        carouselClass = carouselClass.children.length - 1;
+    } else {
+        indexCount = carouselClass.children.length - 1; // Fix: Assign to indexCount, not carouselClass
     }
     updateImg();
     countTimer = 0;
 };
-// timer for next image:
-var countTimer = 0;
+
+// timer for next image
 var timerFunc = function () {
     setInterval(function () {
         countTimer += 1;
@@ -40,8 +43,7 @@ var timerFunc = function () {
         if (countTimer > 5) {
             if (indexCount < carouselClass.children.length - 1) {
                 indexCount++;
-            }
-            else {
+            } else {
                 indexCount = 0;
             }
             updateImg();
@@ -51,6 +53,7 @@ var timerFunc = function () {
     }, 1000);
 };
 timerFunc();
-// eventlinsters
+
+// event listeners
 nextbutton.addEventListener("click", nextImageFunc);
 prevbutton.addEventListener("click", prevImageFunc);

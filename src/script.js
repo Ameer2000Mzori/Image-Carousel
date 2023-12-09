5380f33c6273de78074459e46b657ace7982760b
Ameer <ameerapex496@gmail.com>
2023-12-09 17:57:08 +0100

    added comment / selected elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-console.log("hallo world");
+// selected elements
+var nextbutton = document.getElementsByClassName("next-button")[0];
+var prevbutton = document.getElementsByClassName("prev-button")[0];
+var carousel = document.getElementById("#carousel");
+// functions
+// eventlinsters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,8 @@
-console.log("hallo world");
+// selected elements
+const nextbutton = document.getElementsByClassName("next-button")[0];
+const prevbutton = document.getElementsByClassName("prev-button")[0];
+const carousel = document.getElementById("#carousel");
+
+// functions
+
+// eventlinsters

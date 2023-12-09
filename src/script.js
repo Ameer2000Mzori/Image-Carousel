9379d1850abc227025bc9092a2069359362feff7
Ameer <ameerapex496@gmail.com>
2023-12-09 18:09:36 +0100

    added little styling and + added images + added functions for next and prev

--- src/script.js
+++ src/script.js
@@ -1,6 +1,34 @@
 // selected elements
 var nextbutton = document.getElementsByClassName("next-button")[0];
 var prevbutton = document.getElementsByClassName("prev-button")[0];
-var carousel = document.getElementById("#carousel");
+var carouselClass = document.getElementsByClassName("carousel-Class")[0];
+// gelobal varibales
+var indexCount = 0;
 // functions
+var updateImg = function () {
+    var translateValue = -indexCount * 100 + "%";
+    carouselClass.style.transform = "translateX(".concat(translateValue, ")");
+};
+// next img function
+var nextImageFunc = function () {
+    if (indexCount < carouselClass.children.length - 1) {
+        indexCount++;
+    }
+    else {
+        indexCount = 0;
+    }
+    updateImg();
+};
+// prev img function
+var prevImageFunc = function () {
+    if (indexCount > 0) {
+        indexCount--;
+    }
+    else {
+        carouselClass = carouselClass.children.length - 1;
+    }
+    updateImg();
+};
 // eventlinsters
+nextbutton.addEventListener("click", nextImageFunc);
+prevbutton.addEventListener("click", prevImageFunc);
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,40 @@
 // selected elements
 const nextbutton = document.getElementsByClassName("next-button")[0];
 const prevbutton = document.getElementsByClassName("prev-button")[0];
-const carousel = document.getElementById("#carousel");
+const carouselClass = document.getElementsByClassName(
+  "carousel-Class"
+)[0] as any;
+
+// gelobal varibales
+let indexCount = 0;
 
 // functions
+const updateImg = () => {
+  const translateValue = -indexCount * 100 + "%";
+  carouselClass.style.transform = `translateX(${translateValue})`;
+};
+
+// next img function
+const nextImageFunc = () => {
+  if (indexCount < carouselClass.children.length - 1) {
+    indexCount++;
+  } else {
+    indexCount = 0;
+  }
+  updateImg();
+};
+
+// prev img function
+const prevImageFunc = () => {
+  if (indexCount > 0) {
+    indexCount--;
+  } else {
+    carouselClass = carouselClass.children.length - 1;
+  }
+  updateImg();
+};
 
 // eventlinsters
+
+nextbutton.addEventListener("click", nextImageFunc);
+prevbutton.addEventListener("click", prevImageFunc);
